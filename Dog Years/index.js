5f8cd1c7ed783b0dda0324ca9c4b51177a67a22f
ElaMoscicka <elakorzeniowska@interia.pl>
2017-09-02 12:51:33 +0200

    Create index.js

---
+++ Dog Years/index.js
@@ -0,0 +1,5 @@
+var myAge = 20;
+var firstYears = 2 * 10.5;
+var laterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+console.log('My age in dog years is ' +myAgeInDogYears + '.');
