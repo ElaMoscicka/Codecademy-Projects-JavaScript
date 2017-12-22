c7e4177837b83364eb40bea3688aef02580be239
ElaMoscicka <elakorzeniowska@interia.pl>
2017-12-22 22:13:31 +0100

    Create scriptj.js

---
+++ Dragon Slayer/scriptj.js
@@ -0,0 +1,21 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+while (slaying) {
+  if (youHit) {
+    console.log("You hit the dragon and did " + damageThisRound + " damage!");
+    totalDamage += damageThisRound;
+    
+    if (totalDamage >= 4) {
+      console.log("You did it! You slew the dragon!");
+      slaying = false;
+    } else {
+      youHit = Math.floor(Math.random() * 2);
+    }
+  } else {
+    console.log("The dragon burninates you! You're toast.");
+    slaying = false;
+  }
+}
