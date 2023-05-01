b68ecffc0002e1c25f153d75e3774500fde13fb6
Shpetim Aliu <106196770+shpetimaliu@users.noreply.github.com>
2023-05-02 00:27:13 +0200

    Update script.js
    - hasUppercase(), hasLowercase(), isLongEnough(), and hasSpecialCharacter() return true or false instead of returning undefined or null.
    - hasSpecialCharacter() has been modified to use a regular expression that matches possible special characters.
    - isPasswordValid() has been improved to set valid and invalid conditions in the string and then only print the invalid messages if one or more conditions are not met.

--- Password Validator/script.js
+++ Password Validator/script.js
@@ -1,62 +1,41 @@
-//A function that verifies the password has an uppercase letter.
-function hasUppercase(input){
-  for (var i = 0; i<input.length; i++){
-    if (input[i] === input[i].toUpperCase()){
-    return true;
-		}
-	}
+function hasUppercase(input) {
+  return input !== input.toLowerCase();
 }
 
-//A function that verifies the password has a lowercase letter.
-function hasLowercase(input){
-  for (var i = 0; i<input.length; i++){
-    if (input[i] === input[i].toLowerCase()){
-      return true;
-    }
-  }
+function hasLowercase(input) {
+  return input !== input.toUpperCase();
 }
 
-//A function that verifies the password is at least 8 characters
-function isLongEnough(input){
-  if (input.length >= 8){
-    return true;
-  } 
+function isLongEnough(input) {
+  return input.length >= 8;
 }
 
-//A function that verifies the password has a special character.
-function hasSpecialCharacter(input){
-  var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*'];
-
-  for (var i = 0; i<input.length; i++){
-    for (var j = 0; j<specialCharacters.length; j++){
-    	if (input[i] === specialCharacters[j]){
-      return true;
-    }
-  }
-}
+function hasSpecialCharacter(input) {
+  const specialCharacters = /[!@#$%^&*]/;
+  return specialCharacters.test(input);
 }
 
-//A function that reports to the user if their password is complex enough, and if not, tells them what they are missing.
-function isPasswordValid(input){
-  if (hasUppercase(input) && hasLowercase(input) && isLongEnough(input) && hasSpecialCharacter(input)) {
+function isPasswordValid(input) {
+  const validConditions = [
+    hasUppercase(input),
+    hasLowercase(input),
+    isLongEnough(input),
+    hasSpecialCharacter(input),
+  ];
+
+  const invalidConditions = [
+    !hasUppercase(input) && 'This password needs a capital letter!',
+    !hasLowercase(input) && 'This password needs a lowercase letter!',
+    !isLongEnough(input) && 'Your password needs at least 8 characters!',
+    !hasSpecialCharacter(input) && 'Your password needs a special character!',
+  ];
+
+  if (validConditions.every((condition) => condition)) {
     console.log('This password is valid.');
+  } else {
+    invalidConditions.forEach((condition) => condition && console.log(condition));
   }
-  if (!hasUppercase(input)){
-    console.log('This password needs a capital letter!');
-  }
-  if (!hasLowercase(input)){
-    console.log('This passwords needs a lowercase letter!')
-  }
-  if (!isLongEnough(input)){
-    console.log('Your passwords needs at least 8 letters!')
-  }
-  
-  if (!hasSpecialCharacter(input)) {
-    console.log('Your passwords needs a special character!')
-  }
-  
 }
 
-
-//word we are going to use as a password
+// example usage
 isPasswordValid('HAaHaaaaa');
